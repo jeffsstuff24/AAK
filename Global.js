a870500759f73467cb8d2ba38ebf5926fc0521bc
Zakido <137637073+jeffsstuff24@users.noreply.github.com>
2025-11-27 17:15:03 -0500

    Add files via upload

---
+++ Global.js
@@ -0,0 +1,76 @@
+var aak_rev = "251125-1200";
+
+var BasePath = [];
+initializeArray();
+
+//////////////////////////////////////////////////////////
+// initialize the Array of Links
+//////////////////////////////////////////////////////////
+	
+function initializeArray()
+{
+	BasePath = [];
+	BasePath.push("https://www.gocomics.com/9to5","9 to 5");			
+	BasePath.push("https://www.gocomics.com/andertoons" ,"Andertoons");
+	BasePath.push("https://www.arcamax.com/thefunnies/archie" ,"Archie");
+	BasePath.push("https://www.gocomics.com/baldo" ,"Baldo");
+	BasePath.push("https://www.gocomics.com/bc" ,"B.C.");
+	BasePath.push("https://www.gocomics.com/back-to-bc" ,"Back to B.C.");
+	BasePath.push("https://comicskingdom.com/beetle-bailey-1" ,"Beetle Bailey");
+	BasePath.push("https://www.gocomics.com/ben" ,"Ben");
+	BasePath.push("https://comicskingdom.com/blondie" ,"Blondie");
+	BasePath.push("https://www.gocomics.com/bottomliners" ,"Bottomliners");
+	BasePath.push("https://comicskingdom.com/brilliant-mind-of-edison-lee" ,"Brilliant Mind of Edison");
+	BasePath.push("https://www.gocomics.com/calvinandhobbes" ,"Calvin and Hobbes");
+	BasePath.push("https://www.gocomics.com/closetohome" ,"Close to Home");
+	BasePath.push("https://www.gocomics.com/crankshaft" ,"Crankshaft");
+	BasePath.push("https://comicskingdom.com/crock" ,"Crock");
+	BasePath.push("https://comicskingdom.com/dennis-the-menace" ,"Denise the Menace");
+	BasePath.push("https://www.gocomics.com/drabble" ,"drabble");
+	BasePath.push("https://comicskingdom.com/dustin" ,"Dustin");
+	BasePath.push("https://comicskingdom.com/family-circus" ,"Family Circus");
+	BasePath.push("https://www.gocomics.com/familytree" ,"Family Tree");
+	BasePath.push("https://www.gocomics.com/farcus" ,"Farcus");
+	BasePath.push("https://www.gocomics.com/forbetterorforworse" ,"For Better or Worse");
+	BasePath.push("https://www.gocomics.com/foxtrot" ,"FoxTrot");
+	BasePath.push("https://www.gocomics.com/foxtrotclassics" ,"Foxtrot Classics");
+	BasePath.push("https://www.gocomics.com/freshlysqueezed" ,"Freshly Squeezed");
+	BasePath.push("https://www.gocomics.com/grand-avenue" ,"Grand Avenue");	
+	BasePath.push("https://comicskingdom.com/hagar-the-horrible" ,"Hagar the Horrible");
+	BasePath.push("https://www.gocomics.com/heartofthecity" ,"Heart of the City");
+	BasePath.push("https://www.gocomics.com/herman" ,"Herman");	
+	BasePath.push("https://comicskingdom.com/hi-and-lois" ,"Hi and Lois");	
+	BasePath.push("https://www.gocomics.com/inthebleachers" ,"In the Bleachers");
+	BasePath.push("https://www.gocomics.com/lola" ,"Lola");	
+	BasePath.push("https://www.gocomics.com/looseparts" ,"Loose Parts");
+	BasePath.push("https://www.gocomics.com/luann" ,"Luann");	
+	BasePath.push("https://www.gocomics.com/luann-againn" ,"Luann Again");	
+	BasePath.push("https://www.gocomics.com/moderately-confused" ,"Moderately Confused");
+	BasePath.push("https://www.gocomics.com/nonsequitur" ,"non Sequitur");	
+	BasePath.push("https://www.gocomics.com/offthemark" ,"Off the Mark");
+	BasePath.push("https://comicskingdom.com/pardon-my-planet" ,"Pardon My Planet");
+	BasePath.push("https://www.gocomics.com/peanuts" ,"Peanuts");
+	BasePath.push("https://www.gocomics.com/peanuts-begins" ,"Peanuts Begins");
+	BasePath.push("https://www.gocomics.com/pickles" ,"Pickles");
+	BasePath.push("https://comicskingdom.com/pluggers" ,"Pluggers");
+	BasePath.push("https://www.gocomics.com/realitycheck" ,"Reality Check");	
+	BasePath.push("https://www.gocomics.com/ripleysbelieveitornot" ,"Ripley's Believe It or Not");	
+	BasePath.push("https://www.gocomics.com/roseisrose" ,"Rose Is Rose");
+	BasePath.push("https://www.gocomics.com/shirley-and-son-classics" ,"Shirley and Son Classics");
+	BasePath.push("https://comicskingdom.com/shoe" ,"Shoe");
+	BasePath.push("https://www.gocomics.com/speedbump" ,"Speed Bump");
+	BasePath.push("https://www.gocomics.com/stonesoup" ,"Stone Soup");
+	BasePath.push("https://www.gocomics.com/strangebrew" ,"Strange Brew");	
+	BasePath.push("https://www.gocomics.com/the-born-loser" ,"The Born Loser");
+	BasePath.push("https://www.gocomics.com/theflyingmccoys" ,"The Flying McCoys");
+	BasePath.push("https://www.gocomics.com/toomuchcoffeeman" ,"Too Much Coffee Man");
+	BasePath.push("https://www.gocomics.com/lockhorns" ,"The Lockhorns");
+	BasePath.push("https://www.gocomics.com/themiddletons" ,"The middletons");	
+	BasePath.push("https://www.gocomics.com/wide-open", "Wide Open");
+	BasePath.push("https://www.gocomics.com/wizardofid" ,"Wizard of ID");	
+	BasePath.push("https://www.gocomics.com/wizard-of-id-classics" ,"Wizard of ID Classics");	
+	BasePath.push("https://www.gocomics.com/workingitout" ,"Working it Out");
+	BasePath.push("https://www.gocomics.com/wumo" ,"WuMo");
+	BasePath.push("https://www.gocomics.com/zackhill" ,"Zack Hill");
+	BasePath.push("https://comicskingdom.com/zits" ,"Zits");
+}
