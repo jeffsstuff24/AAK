cc4bfb638bd2d54b393b47c339498b9bcf70e7f0
Zakido <137637073+jeffsstuff24@users.noreply.github.com>
2026-02-17 12:26:45 -0500

    Update Global.js

--- Global.js
+++ Global.js
@@ -1,4 +1,4 @@
-var aak_rev = "260217-1223";
+var aak_rev = "260217-1226";
 
 var BasePath = [];
 initializeArray();
@@ -99,6 +99,7 @@ function initializeArray()
 }
 
 console.log("global done");
+
 
 
 
